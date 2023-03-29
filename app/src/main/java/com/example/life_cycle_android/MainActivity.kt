6c3a36ae3097e1f60c6ed10ce0b1401ff4db2f08
Luigi-Avila <l.avila.uvg@gmail.com>
2023-03-28 18:04:34 -0600

    feat: resources free in the lifecycle

--- app/src/main/java/com/example/life_cycle_android/MainActivity.kt
+++ app/src/main/java/com/example/life_cycle_android/MainActivity.kt
@@ -8,33 +8,40 @@ import android.util.Log
 class MainActivity : AppCompatActivity() {
 
     private var mediaPlayer: MediaPlayer? = null
-    private val position: Int = 0
+    private var position: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mediaPlayer = MediaPlayer.create(this, R.raw.ai_2)
     }
 
     override fun onStart() {
         super.onStart()
-        mediaPlayer?.start()
+        mediaPlayer = MediaPlayer.create(this, R.raw.ai_2)
         Log.i("LifeCycle", "OnStart")
     }
 
     override fun onResume() {
         super.onResume()
+        mediaPlayer?.seekTo(position)
+        mediaPlayer?.start()
         Log.i("LifeCycle", "onResume")
     }
 
     override fun onPause() {
         super.onPause()
         mediaPlayer?.pause()
+        if (mediaPlayer != null){
+            position = mediaPlayer!!.currentPosition
+        }
         Log.i("LifeCycle", "onPause")
     }
 
     override fun onStop() {
         super.onStop()
+        mediaPlayer?.stop()
+        mediaPlayer?.release()
+        mediaPlayer = null
         Log.i("LifeCycle", "onStop")
     }
 
